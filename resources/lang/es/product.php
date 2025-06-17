25f2543160192ed7c6aa9e25e6f0661d021920b6
GitHub <action@github.com>
2025-06-17 22:59:17 +0000

    Allow Media Relation Manager to be extended by using getXXXDefault() … (#2192)

--- resources/lang/es/product.php
+++ resources/lang/es/product.php
@@ -6,6 +6,10 @@ return [
 
     'plural_label' => 'Productos',
 
+    'tabs' => [
+        'all' => 'Todo',
+    ],
+
     'status' => [
         'unpublished' => [
             'content' => 'Actualmente en estado de borrador, este producto está oculto en todos los canales y grupos de clientes.',
@@ -77,6 +81,7 @@ return [
         ],
         'tags' => [
             'label' => 'Etiquetas',
+            'helper_text' => 'Separa las etiquetas presionando Enter, Tab o coma (,)',
         ],
         'collections' => [
             'label' => 'Colecciones',
@@ -86,6 +91,9 @@ return [
     'pages' => [
         'availability' => [
             'label' => 'Disponibilidad',
+        ],
+        'edit' => [
+            'title' => 'Información Básica',
         ],
         'identifiers' => [
             'label' => 'Identificadores del Producto',
